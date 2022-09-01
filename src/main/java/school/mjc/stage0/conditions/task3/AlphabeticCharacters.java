cf75608b9da4d6c1c8878e39cc85c515f7322c75
v-nebe <katya.shauneva@gmail.com>
2022-09-01 17:54:43 +0300

    tasks have been completed

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,9 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
-        if (((int) character >= 65 && (int) character <= 90) || ((int) character >= 97 && (int) character <= 122))
-            System.out.println("English");
+        if (character == 'A' || character =='a' || character == 'E' || character == 'e' || character=='O' ||
+        character == 'o' || character == 'I' || character == 'i' || character == 'U' || character == 'u')
+            System.out.println("Vowel");
+        else if (((int) character >= 65 && (int) character <= 90) || ((int) character >= 97 && (int) character <= 122))
+            System.out.println("Consonant");
         else
-            System.out.println("Non English");
+            System.out.println("wrong alphabet!");
     }
 }
